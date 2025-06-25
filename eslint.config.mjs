a4dc8f2c0f64ac531d599ef9ce18ab3d51e28490
Vijay Singh <vijay.singh@adgonline.in>
2025-06-25 11:10:23 +0530

    signin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",  // Disable the 'react/no-unescaped-entities' rule
+      "@next/next/no-page-custom-font": "off",  // Disable the '@next/next/no-page-custom-font' rule
+    },
+  },
 ];
 
 export default eslintConfig;
