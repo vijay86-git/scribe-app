cc2d1b6443d1aa3890b136125d9dc3c5aa68dbc2
Vijay Singh <vijay.singh@adgonline.in>
2025-06-27 19:57:05 +0530

    changes

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 // /lib/api.ts
-const API_BASE_URL = "http://127.0.0.1:8001/api/v1";//process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function apiFetch<T>(
   endpoint: string,
