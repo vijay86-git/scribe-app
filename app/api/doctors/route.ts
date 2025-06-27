2f5efa910fa626d5679fa97c04d2d5356bc75745
Vijay Singh <vijay.singh@adgonline.in>
2025-06-27 18:24:19 +0530

    changes

--- app/api/doctors/route.ts
+++ app/api/doctors/route.ts
@@ -8,12 +8,17 @@ export async function POST(req: Request) {
         const body = await req.json();
         const cookie = (await cookies()).get('session')?.value;
         const session = await decrypt(cookie);
+
+        if (!session || !session.token) {
+          return NextResponse.json({ success: false, msg: { message: "Unauthorized" } }, { status: 401 });
+        }
+
         try {
                 const api_url: string = `${apiRoutes.doctors}?page=${body.page}&q=${body.q}`;
                 const resp: unknown = await apiFetch(api_url, {
                     method: 'GET',
                     headers: {
-                              Authorization: `Bearer ${session.token}`,
+                              Authorization: `Bearer ${session?.token}`,
                               'X-Custom-Header': 'value',
                     },
                 });
