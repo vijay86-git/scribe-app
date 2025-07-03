95407517f73474c92ac04dad7ab6164562b94c38
Vijay Singh <vijay.singh@adgonline.in>
2025-07-03 18:16:02 +0530

    update

--- components/clinic/optional.tsx
+++ components/clinic/optional.tsx
@@ -108,24 +108,26 @@ export default function Optional({designations, specializations, clinic_detail}:
 
                           </div>
                        </div>
+
                        <div className="flex gap-3 mb-3">
                           <div className="grid w-full max-w-sm items-center gap-1.5">
-                             <Label htmlFor="designation">Designation/Role</Label>
-                             {designations && designations.length > 0 && (
-                                    <Select value={formData.designation}>
-                                      <SelectTrigger className="w-full">
-                                        <SelectValue placeholder="Select a designation" />
-                                      </SelectTrigger>
-                                      <SelectContent>
-                                        {designations.map((item, idx) => (
-                                          <SelectItem key={idx} value={String(item.id)}>
-                                            {item.name}
-                                          </SelectItem>
-                                        ))}
-                                      </SelectContent>
-                                    </Select>
-                             )}
+                                <Label htmlFor="designation">Designation/Role</Label>
+                                {designations && designations.length > 0 && (
+                                  <Select value={formData.designation}>
+                                    <SelectTrigger className="w-full">
+                                      <SelectValue placeholder="Select a designation" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                      {designations.map((item, idx) => (
+                                        <SelectItem key={idx} value={String(item.id)}>
+                                          {item.name}
+                                        </SelectItem>
+                                      ))}
+                                    </SelectContent>
+                                  </Select>
+                               )}
                           </div>
+
                           <div className="grid w-full max-w-sm items-center gap-1.5">
                              <Label htmlFor="website_clinic_url">Website or Clinic URL <sup>*</sup></Label>
                              <Input type="url" id="url" onChange={handleChange} placeholder="Website or Clinic URL" name="website_clinic_url" value={formData.website_clinic_url} />
@@ -147,6 +149,7 @@ export default function Optional({designations, specializations, clinic_detail}:
                                 </SelectContent>
                              </Select>
                           </div>
+
                        </div>
 
                        <div className="flex gap-3"><Button className="mt-3">Save Changes</Button></div>
