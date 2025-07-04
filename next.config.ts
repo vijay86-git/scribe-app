430632c146d94eafa8bea853f3910e397f2f30e6
Vijay Singh <vijay.singh@adgonline.in>
2025-07-04 16:48:25 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('http://127.0.0.1:8001/storage/uploads/**')],
+  },
 };
 
 export default nextConfig;
