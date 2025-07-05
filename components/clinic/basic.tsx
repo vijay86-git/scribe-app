834e3fbe34e9d06268b4901693d844ec0ea431a4
Vijay Singh <vijay.singh@adgonline.in>
2025-07-05 16:41:36 +0530

    update

--- components/clinic/basic.tsx
+++ components/clinic/basic.tsx
@@ -306,7 +306,7 @@ export default function Basic({countries, clinic_detail}: {countries: MetaCol1[]
 	                          <div className="grid w-full max-w-sm items-center gap-1.5">
 	                             <Label htmlFor="clinic_logo">Clinic Logo<sup>*</sup></Label>
 	                             <Input id="image" type="file" name="upload_clinic_logo" accept="image/*" onChange={handleImageChange} />
-	                             <Image alt="" width={120} height={0} layout="intrinsic" src={`http://127.0.0.1:8001/storage/uploads/${clinic_logo}`} />
+	                             <Image alt="" width={120} height={0} layout="intrinsic" src={clinic_logo} />
 
 	                             { errors.upload_clinic_logo && (
 						                  	   <p className="text-red-500 text-xs">{errors.upload_clinic_logo[0]}</p>
