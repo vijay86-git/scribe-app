60acadd031a1e1c5a5bc23e99b57319bd25f5314
Vijay Singh <vijay.singh@adgonline.in>
2025-07-05 14:07:31 +0530

    update

--- schemas/clinic-profile-optional-schema.ts
+++ schemas/clinic-profile-optional-schema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 export const clinicProfileOptionalFormSchema = z.object({
-	  no_of_doctors: z.number().optional(),
-	  daily_monthly_patient_footfall: z.number().optional(),
+	  no_of_doctors: z.string().optional(),
+	  daily_monthly_patient_footfall: z.string().optional(),
 	  designation: z.string().optional(),
 	  website_clinic_url: z.string().optional(),
-	  year_establishment: z.	().optional(),
+	  year_establishment: z.string().optional(),
 	  ai_filter: z.number().optional(),
 });
 
