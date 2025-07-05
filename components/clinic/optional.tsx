8f3cadfb0f8a64e82a6501fca8d6dfd25a527470
Vijay Singh <vijay.singh@adgonline.in>
2025-07-05 13:51:21 +0530

    update

--- components/clinic/optional.tsx
+++ components/clinic/optional.tsx
@@ -77,7 +77,8 @@ export default function Optional({designations, specializations, clinic_detail}:
 
   const [formData, setFormData] = useState<FormData>({no_of_doctors, daily_monthly_patient_footfall, designation, website_clinic_url, year_establishment, ai_filter});
 
-  const [selectedSpecializations, setSelectedSpecializations] = useState<number[] | (() => number[])>(clinic_detail.specializations ?? []);
+  const [selectedSpecializations, setSelectedSpecializations] = useState<number[]>(clinic_detail.specializations?.map(spec => spec.value) ?? []); 
+  //useState<number[] | (() => number[])>(clinic_detail.specializations ?? []);
 
   const [errors, setErrors] = useState<FormErrors>({});
 
