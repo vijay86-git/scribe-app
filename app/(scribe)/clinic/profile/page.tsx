7f92d19f5a2beeb86b023d4c312bb25527f641e2
Vijay Singh <vijay.singh@adgonline.in>
2025-07-05 13:23:34 +0530

    update

--- app/(scribe)/clinic/profile/page.tsx
+++ app/(scribe)/clinic/profile/page.tsx
@@ -63,10 +63,10 @@ type ClinicType = {
     image?: string;
     no_of_doctors?: number;
     daily_monthly_patient_footfall?: number;
-    designation?: string;
+    designation?: number;
     website_clinic_url?: string;
-    year_establishment?: string;
-    ai_filter?: string;
+    year_establishment?: number;
+    ai_filter?: number;
 };
 
 export default async function Page() {
