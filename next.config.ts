cc9ff8fd7c9be308cf41e4593d41d5fbf7257537
Vijay Singh <vijay.singh@adgonline.in>
2025-07-05 16:32:07 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,17 @@ import type { NextConfig } from "next";
 const imageDomain = process.env.NEXT_PUBLIC_IMAGE_DOMAIN || 'https://api.adgscribe.companydemo.ca';
 const parsed = new URL(imageDomain);
 
+const remotePattern = {
+  protocol: parsed.protocol.replace(':', ''),
+  hostname: parsed.hostname,
+  pathname: '/storage/uploads/**',
+  ...(parsed.port ? { port: parsed.port } : {}), // only include port if it exists
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: parsed.protocol.replace(':', ''),
-        hostname: parsed.hostname,
-        port: parsed.port || '',
-        pathname: '/storage/uploads/**', // wildcard path
-      },
-    ],
+    remotePatterns: [remotePattern],
   },
 };
 
